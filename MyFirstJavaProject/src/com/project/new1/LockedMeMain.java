3421ec31f6d32e07a8cf73be210dd6bd486402e1
Amritpal Singh <79758080+amritpalsingh85419@users.noreply.github.com>
2022-12-30 14:51:12 +0530

    MyFirstProject

---
+++ MyFirstJavaProject/src/com/project/new1/LockedMeMain.java
@@ -0,0 +1,17 @@
+package com.project.new1;
+
+public class LockedMeMain {
+
+	public static void main(String[] args) {
+		
+		// Create "main" folder if not present in current folder structure
+		FileOperations.createMainFolderIfNotPresent("main");
+		
+		MenuOptions.printWelcomeScreen("LockedMe", "Sitansu Subudhi");
+		
+		HandleOptions.handleWelcomeScreenInput();
+	}
+
+	
+}
+
